cf474795d5139582080e0d7521dbac7b4b053019
MateusHuebra <mateushuebra@gmail.com>
2021-01-18 15:36:45 -0300

    add photo icon placeholder to navbar

--- View/Layout/header.php
+++ View/Layout/header.php
@@ -6,7 +6,7 @@
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!--Import materialize.css-->
 	<link type="text/css" rel="stylesheet" href="/photosApp/node_modules/materialize-css/dist/css/materialize.min.css"  media="screen,projection"/>
-	<link rel="stylesheet" type="text/css" href="/photosApp/css/login.css">
+	<link rel="stylesheet" type="text/css" href="/photosApp/css/index.css">
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
@@ -15,6 +15,17 @@
 	<nav>
 		<div class="nav-wrapper indigo darken-4">
 			<a href="#!" class="brand-logo center">photosApp</a>
-
+			<!--
+			<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+			-->
+			<a href="#" data-target="mobile-demo" class="sidenav-trigger"><img src="/photosApp/images/default.png" alt="" class="circle-mobile"></a>
+			<ul class="left hide-on-med-and-down">
+				<li><img src="/photosApp/images/default.png" alt="" class="circle"></li>
+				<li><a href="sass.html">Sass</a></li>
+			</ul>
 		</div>
 	</nav>
+
+	<ul class="sidenav" id="mobile-demo">
+	    <li><a href="sass.html">Sass</a></li>
+ 	</ul>
