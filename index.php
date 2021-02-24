b572b7086a2af0b494aad1286d3856fe2236e503
MateusHuebra <mateushuebra@gmail.com>
2021-02-24 19:35:49 -0300

    fixed url reading when GET is passed

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ spl_autoload_register(function ($class_name) {
     require str_replace('\\', '/', $class_name).'.php';
 });
 
-$requestUri = explode('/', $_SERVER['REQUEST_URI']);
+$requestUri = explode('?', $_SERVER['REQUEST_URI']);
+$requestUri = explode('/', $requestUri[0]);
 $className = 'Controller\\'.ucfirst($requestUri[1]);
 if(isset($requestUri[2]) && !preg_match("/^[?]/", $requestUri[2])) {
 	$methodName = $requestUri[2];
